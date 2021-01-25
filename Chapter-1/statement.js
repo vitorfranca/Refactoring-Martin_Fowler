d2474c90aa1ef167e5aebd347415e3b846962276
Vitor França <github@vitorfranca.com>
2021-01-24 22:31:22 -0300

    Slide Statement (252)

--- Chapter-1/statement.js
+++ Chapter-1/statement.js
@@ -1,6 +1,5 @@
 function statement(invoice, plays) {
   let totalAmount = 0;
-  let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
 
   for (let perf of invoice.performances) {
@@ -8,6 +7,8 @@ function statement(invoice, plays) {
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
     totalAmount += amountFor(perf);
   }
+
+  let volumeCredits = 0;
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
   }
