9b79b9e262df76bdc90a8c0f22885e6e2da94c1e
Vitor França <github@vitorfranca.com>
2021-01-24 22:04:03 -0300

    Replace Temp with Query (207) playFor

--- Chapter-1/statement.js
+++ Chapter-1/statement.js
@@ -9,7 +9,7 @@ function statement(invoice, plays) {
   }).format;
 
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
+    const play = playFor(perf);
     let thisAmount = amountFor(perf, play);
 
     // add credits by volume
@@ -48,6 +48,10 @@ function statement(invoice, plays) {
     }
 
     return result;
+  }
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
   }
 }
 
