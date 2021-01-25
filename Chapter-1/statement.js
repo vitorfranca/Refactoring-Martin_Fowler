087b836f045f5bdaf2940b05dd7f25c04e92c71b
Vitor França <github@vitorfranca.com>
2021-01-24 22:37:03 -0300

    Rename Variable

--- Chapter-1/statement.js
+++ Chapter-1/statement.js
@@ -51,18 +51,18 @@ function statement(invoice, plays) {
     }).format(aNumber / 100);
   }
   function totalVolumeCredits() {
-    let volumeCredits = 0;
+    let result = 0;
     for (let perf of invoice.performances) {
-      volumeCredits += volumeCreditsFor(perf);
+      result += volumeCreditsFor(perf);
     }
-    return volumeCredits;
+    return result;
   }
   function appleSauce() {
-    let totalAmount = 0;
+    let result = 0;
     for (let perf of invoice.performances) {
-      totalAmount += amountFor(perf);
+      result += amountFor(perf);
     }
-    return totalAmount;
+    return result;
   }
 }
 
